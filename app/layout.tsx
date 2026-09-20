df9f11ec994b72a16bdf459232dbfa50473c0c7c
Aditya Sharma <riashab119@gmail.com>
2026-09-20 17:57:39 +0530

    Implement AI code generation feature and integrate Google GenAI API

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import { cn } from "@/lib/utils";
 import { ThemeProvider } from "@/components/theme-provider";
 import Header from "@/components/Header";
 import { ClerkProvider } from "@clerk/nextjs";
+import { Toaster } from "sonner";
 
 const lora = Lora({
   subsets: ["latin"],
@@ -42,6 +43,8 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
 
             <Header></Header>
             <main>{children}</main>
+
+            <Toaster richColors/>
         </ThemeProvider>
         </body>
     </html>
